bd40806e268fdc11532384eea84e1ceb52aae945
Mika <Apnexion@gmail.com>
2019-07-07 01:58:30 +0300

    Began implementing basic gameplay functionality

---
+++ Break Those Bricks/Assets/Scripts/Paddle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    // Configuration parameters
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float minX = 0.94f;
+    [SerializeField] float maxX = 15.07f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
+        transform.position = paddlePos;
+    }
+}
